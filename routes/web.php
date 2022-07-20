9d5856a0534de91d4197469ebb5b76d25d7c0f93
DanielHofer2001 <hoferd45@gmail.com>
2022-07-20 19:52:52 -0400

    Cambio ruta

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 /* OBTENCION DE EL HTML DE WELCOME */
 Route::get('/', function () {
-    return view('auth.login');
+    return ('welcome');
 });
 
 /* OBTENCION DE EL HTML DE LA PAGINA DE INICIO */
