8375c70023e297c7893e3188ac5dff50c1e3491e
DanielHofer2001 <hoferd45@gmail.com>
2022-07-22 04:10:13 -0400

    Actualizacion 22/07/2022

--- resources/views/prueba1/index.blade.php
+++ resources/views/prueba1/index.blade.php
@@ -96,13 +96,18 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Agende su hora</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
             </div>
 
             <div class="modal-body">
+
+                <div class="form-group">
+                  <label for="id"></label>
+                  <input type="hidden" class="form-control" name="id" id="id" aria-describedby="helpId" placeholder="">
+                </div>
 
                 <div class="from-group">
                 <label for="nombre">nombre</label>
