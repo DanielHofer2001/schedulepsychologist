d1390e47aebf63a609f852c4d883969d42ac33e3
DanielHofer2001 <hoferd45@gmail.com>
2022-07-22 04:15:39 -0400

    Actualizacion mas reciente

--- database/migrations/2022_07_21_214946_clientes_table.php
+++ database/migrations/2022_07_21_214946_clientes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         //
         Schema::create('clientes', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('nombre');
             $table->string('apellido');
             $table->integer('telefono');
